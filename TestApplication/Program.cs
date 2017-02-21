687289f3d111914dc945f1755efe2e151caac209
Mike <mikekh@uw.edu>
2017-02-21 14:18:17 -0800

    Setup an array and test some methods.

---
+++ TestApplication/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[7];
+
+            //assigns an array
+            numbers[0] = 4;
+            numbers[1] = 5;
+            numbers[2] = 82723;
+            numbers[3] = 234;
+            numbers[4] = 23;
+
+            //Shows length of array
+            Console.WriteLine("Length of Numbers Array: " + numbers.Length);
+            Console.WriteLine("Numbers in Array");
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            //Reverse Array
+            System.Array.Reverse(numbers);
+            Console.WriteLine("------");
+            Console.WriteLine("Numbers in reverse.");
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
