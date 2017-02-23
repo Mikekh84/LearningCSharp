13ced5d07f05b4afc026d87fdfe6d3503758ccc6
Mike <mikekh@uw.edu>
2017-02-22 17:25:52 -0800

    Created a simple while loos

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -8,8 +8,8 @@ namespace TestApplication
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
+            int i = 0;
            float number = 7.5f;
            if (number > 5 )
             {
@@ -22,6 +22,12 @@ namespace TestApplication
            else
             {
                 Console.WriteLine("This is equal");
+            }
+
+           while (i < 5) {
+                Console.WriteLine(i);
+                i++;
+
             }
         }
     }
