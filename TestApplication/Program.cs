3993d0c7f92d2d2e80832572729715b3bec0939a
Mike <mikekh@uw.edu>
2017-02-22 16:18:29 -0800

    Added a float and wrote conditionals to check great less than and equal.

--- TestApplication/Program.cs
+++ TestApplication/Program.cs
@@ -8,10 +8,21 @@ namespace TestApplication
 {
     class Program
     {
-      
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a test.");
+           float number = 7.5f;
+           if (number > 5 )
+            {
+                Console.WriteLine("This is greater than 5.");
+            }
+           else if (number < 5)
+            {
+                Console.WriteLine("This is less than 5");
+            }
+           else
+            {
+                Console.WriteLine("This is equal");
+            }
         }
     }
 }
